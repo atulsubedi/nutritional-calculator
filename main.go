946eb7b6af0382e63c2857f2aa8cff247b678c4c
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-12-15 21:09:44 +0545

    fix main.go

--- main.go
+++ main.go
@@ -14,5 +14,7 @@ func main() {
 	}, Food)
 
 	fmt.Printf("Nutritional Score:%d\n", ns.value)
+	
+	// The below getnutriscore() will give you the grade of your food intake
 	fmt.Printf("Nutri Score: %s\n", ns.GetNutriScore())
 }
